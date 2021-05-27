353a3055bd61cfd13c108445e7e1da11aca4a709
tomatopickle <65482369+tomatopickle@users.noreply.github.com>
2021-05-27 09:47:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var http = require("http");
-
+const express = require('express');
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Heroku world!");
